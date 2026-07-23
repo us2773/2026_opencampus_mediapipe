bd91bcd894a130f7e32be8a94721168999eb1c5b
Kushina <superaoba0602@gmail.com>
2026-07-23 15:53:41 +0900

    fix(action): 履歴判定時のdequeスライスを修正

--- actions/jump.py
+++ actions/jump.py
@@ -9,9 +9,10 @@ class JumpDetector:
         self.frames.append(landmarks[23][1])
         if len(self.frames) < self.frames.maxlen:
             return False
-        left_foot = sum(self.frames[0:3]) / 3
-        left_side = sum(self.frames[5:8]) / 3
-        top = sum(self.frames[10:13]) / 3
-        right_side = sum(self.frames[15:18]) / 3
-        right_foot = sum(self.frames[20:23]) / 3
+        frames = list(self.frames)
+        left_foot = sum(frames[0:3]) / 3
+        left_side = sum(frames[5:8]) / 3
+        top = sum(frames[10:13]) / 3
+        right_side = sum(frames[15:18]) / 3
+        right_foot = sum(frames[20:23]) / 3
         return left_foot > left_side > top < right_side < right_foot and left_foot - top >= 0.05
--- actions/swing.py
+++ actions/swing.py
@@ -9,7 +9,8 @@ class SwingDetector:
         self.frames.append((landmarks[15][1] + landmarks[16][1]) / 2)
         if len(self.frames) < self.frames.maxlen:
             return False
-        top = sum(self.frames[0:3]) / 3
-        middle = sum(self.frames[6:9]) / 3
-        bottom = sum(self.frames[12:15]) / 3
+        frames = list(self.frames)
+        top = sum(frames[0:3]) / 3
+        middle = sum(frames[6:9]) / 3
+        bottom = sum(frames[12:15]) / 3
         return top < middle < bottom and bottom - top >= 0.1
